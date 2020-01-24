a65e0ccae141577f3b1a488af9a448d71cbd86b8
Noam <noams@epd-medical.com>
2020-01-25 00:29:00 +0200

    removing rel_paths.txt on time

--- code/Utils/create_filename_list.py
+++ code/Utils/create_filename_list.py
@@ -5,16 +5,16 @@ from os.path import relpath
 
 
 def file_names_in_tree_root(treeroot, create_file_dir, file_name):
+    file_path = os.path.join(create_file_dir, file_name)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+
     paths = []
     for filename in glob.iglob(treeroot + '**/**', recursive=True):
         if os.path.isfile(os.path.join(treeroot, filename)):
             paths.append(relpath(filename.split('.')[0], treeroot))
 
     paths = sorted(paths)
-
-    file_path = os.path.join(create_file_dir, file_name)
-    if os.path.isfile(file_path):
-        os.remove(file_path)
 
     with open(file_path, 'w') as f:
         for path in paths:
