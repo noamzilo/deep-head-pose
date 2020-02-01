3ba103f6230173c41c49fff89f594c94ca4da11c
Noam <noams@epd-medical.com>
2020-02-01 19:37:14 +0200

    nothing

--- code/Utils/create_filename_list.py
+++ code/Utils/create_filename_list.py
@@ -26,7 +26,7 @@ def file_names_in_tree_root(treeroot, create_file_dir, file_name):
 if __name__ == "__main__":
     def main():
         test_path = r"C:\Noam\Code\vision_course\downloads\datasets\300W-LP\big_set\300W_LP"
-        create_at = ""
+        create_at = test_path
         file_name = "rel_paths.txt"
         paths = file_names_in_tree_root(test_path, create_at, file_name)
         print(paths)
